e38df35e6454b0bae6fa9ad6e42272301fabc16d
danbenosem <danielbenjami562@gmail.com>
2026-05-19 17:28:14 +0100

    task

--- dayTwo/python/level5/function.py
+++ dayTwo/python/level5/function.py
@@ -5,18 +5,18 @@ array=[5,-9,3,-6,2,-11]
 
 
 
-def negative_removed(array[]):
+def negative_removed(array):
 
     
     newArr=  []
 
-    for index in len(array):
+    for index in range( len(array)):
 
-        if(array[index]<0):
+        if array[index]<0:
 
-            newArr[index].append("0");
+            newArr.append("0");
         else:
-            newArr[index]=array[index];
+            newArr.append(array[index]);
 
 
     return newArr;
