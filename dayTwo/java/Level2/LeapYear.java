45a1bb7fcd4b292b538ab206ae554a75aad118b4
danbenosem <danielbenjami562@gmail.com>
2026-05-19 12:58:49 +0100

    task

---
+++ dayTwo/java/Level2/LeapYear.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class LeapYear{
+
+
+public static void main (String [] args){
+
+    Scanner inputCollector= new Scanner(System.in);
+
+
+    System.out.print("enter the year:");
+
+  int year = inputCollector.nextInt();
+
+ if((year % 4 == 0 && year % 100 !=0) && (year % 400==0 && year % 100 ==0)) {
+
+System.out.println("it is a leap year");
+
+} else{
+
+System.out.println("not leap year");
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
